daef0561b22be9f84405e8e6f34b62b7a6253293
Paul <qamunyamaina950@gmail.com>
2025-02-14 14:58:55 +0300

    Validating both name and age input

--- name.py
+++ name.py
@@ -1,2 +1,8 @@
 name = input("Enter your name: ").capitalize()
 age = input("Enter your name: ")
+while True:
+    if (name.isascii and age.isdigit): 
+        print(f"Hello, {name}, you are {age} old")
+        break
+    else:
+        print("Name or age is incorrect")
